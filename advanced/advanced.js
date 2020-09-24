1471dab4f674741b81a3b732d91d676f9010c3a3
Katharina Simma <katharina.simma@diebin.at>
2020-09-24 15:21:17 +0200

    almost finish advanced

--- advanced/advanced.js
+++ advanced/advanced.js
@@ -1,3 +1,18 @@
 console.log(mangas);
 var mangas = JSON.parse(mangas);
 console.table(mangas);
+
+//document.write(mangas[0].author);
+
+for (i=0; i<mangas.length; i++){
+    document.getElementById("result").innerHTML += `
+    <p> My favorite manga is ${mangas[i].title}!</p>
+        <li> ${mangas[i].author}</li>
+        <li> ${mangas[i].read} </li>
+        <div>
+            <img src="${mangas[i].image}" width="250px">
+        </div>
+    <br> `
+    console.log(mangas[i].image)
+};
+
